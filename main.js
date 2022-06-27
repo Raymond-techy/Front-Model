286f4223b617759c338b74f3dda00ce158ef3b62
Raymond-techy <adunoloyerahmantaiwo200208@gmail.com>
2022-06-27 02:54:18 +0100

    initiial commit

--- main.js
+++ main.js
@@ -35,3 +35,30 @@ closeBtn.addEventListener("click", () => {
   closeBtn.style.color = "red";
   openBtn.style.display = "inline-block";
 });
+
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+//select tab content items
+function selectItem() {
+  //remove border
+  removeBorder();
+  removeShow();
+  this.classList.add("tab-border");
+  //Grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  //Add show class
+  tabContentItem.classList.add("show");
+}
+
+//listen for tab click
+tabItems.forEach((item) => item.addEventListener("click", selectItem));
+
+//remove border
+function removeBorder() {
+  tabItems.forEach((item) => item.classList.remove("tab-border"));
+}
+
+function removeShow() {
+  tabContentItems.forEach((item) => item.classList.remove("show"));
+}
